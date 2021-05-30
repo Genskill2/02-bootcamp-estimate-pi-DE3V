c6b9e9cc084646a85022445027ddb5894440d451
Sanjay Kumar <sanjaysreenivas257@gmail.com>
2021-05-30 23:45:26 +0530

    Assignment 1

--- estimate.py
+++ estimate.py
@@ -30,3 +30,44 @@ class TestMC(unittest.TestCase):
     
 if __name__ == "__main__":
     unittest.main()
+    
+    
+    
+    
+    
+def wallis(n):
+ result=1;
+ a=1
+ 
+ 
+ for i in range(1,n):
+               a = 4*i**2/(4*i**2-1)
+               result = result*a
+             
+ result = result*2 
+                                                    
+ return result
+ 
+ 
+ 
+ 
+ 
+def monte_carlo(n):
+ ins = 0
+ result=1
+
+ for i in range(0, n):
+ 
+        x = random.random()**2
+        y = random.random()**2
+    
+        if math.sqrt(x + y) < 1.0:
+           ins += 1
+           
+
+
+ result *= (float(ins) / n) * 4
+ 
+ return result
+
+
